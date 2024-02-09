7c1795f5f15a0008151dc599c241c55579f0f40c
Alex <antuturrru@gmail.com>
2024-02-09 11:30:53 +0300

    Готовый проект + gitignore

--- Mindbox/main.swift
+++ Mindbox/main.swift
@@ -1,11 +1,40 @@
-//
-//  main.swift
-//  Mindbox
-//
-//  Created by Александр Акимов on 09.02.2024.
-//
-
 import Foundation
 
-print("Hello, World!")
+struct GeometryCalculator {
+    
+    static func roundArea(_ radius: Double) -> Double {
+        return Double.pi * pow(radius, 2)
+    }
+    
+    static func triangleArea(firstSide a: Double, secondSide b: Double, thirdSide c: Double) -> Double {
+        let p = (a + b + c) / 2
+        return sqrt(p * (p - a) * (p - b) * (p - c))
+    }
+    
+    static func isRightTriangle(firstSide a: Double, secondSide b: Double, thirdSide c: Double) -> Bool {
+        let sides = [a, b, c].sorted()
+        return (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2])
+    }
+}
+print("Введите числа через пробел")
+let inputStr = readLine()
+
+let numbers = inputStr?.split(separator: " ")
+
+if let numbers = numbers {
+    switch numbers.count {
+    case 1:
+        print("Площадь круга: \(GeometryCalculator.roundArea(Double(numbers[0]) ?? 0))")
+    case 3:
+        print("Площадь треугольника: \(GeometryCalculator.triangleArea(firstSide: Double(numbers[0]) ?? 0, secondSide: Double(numbers[1]) ?? 0, thirdSide: Double(numbers[2]) ?? 0))")
+        if GeometryCalculator.isRightTriangle(firstSide: Double(numbers[0]) ?? 0, secondSide: Double(numbers[1]) ?? 0, thirdSide: Double(numbers[2]) ?? 0){
+            print("Треугольник прямоугольный")
+        }else {
+            print("Треугольник НЕ прямоугольный")
+        }
+    default:
+        print("Неверное количество чисел при вводе")
+    }
+}
+
 
